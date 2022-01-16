d232f6d52884cb1d1768e8db7f4de22046111600
Flechazo <94276146+FlechazoPh@users.noreply.github.com>
2022-01-16 16:19:31 +0800

    Create BuyHack.js

---
+++ QuantumultX/Scripts/BuyHack.js
@@ -0,0 +1,2 @@
+#buyhack
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://ghproxy.com/https://raw.githubusercontent.com/langkhach270389/Quantumult-X-LK/master/Scripts/langkhach/verify_receipt.js
