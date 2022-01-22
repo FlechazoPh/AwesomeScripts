022a22cff4d6bb33d3266fd65e6aa9b8da67c77c
Flechazo <94276146+FlechazoPh@users.noreply.github.com>
2022-01-23 02:30:25 +0800

    Create 10000.py

---
+++ Scripts/CheckIn/10000.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+import base64
+import requests,json
+import time
+from Crypto.Cipher import AES
+from binascii import b2a_hex, a2b_hex
+from Crypto.Util.Padding import pad
+from notify_mtr import send
+
+# pip install pycryptodome
+# 默认喂食
+config_list = [
+   {"mobile": os.environ["TELECOM_MOBILE"], "food": True}
+]
+
+msg = []
+
+
+
+def encrypt(text):
+    key = '34d7cb0bcdf07523'.encode('utf-8')
+
+    cipher = AES.new(key, AES.MODE_ECB)
+    pad_pkcs7 = pad(text.encode('utf-8'), AES.block_size, style='pkcs7')  # 选择pkcs7补全
+    cipher_text = cipher.encrypt(pad_pkcs7)
+
+    return b2a_hex(cipher_text)
+
+def telecom_task(config):
+    mobile = config['mobile']
+    msg.append(mobile + " 开始执行任务...")
+    h5_headers = get_h5_headers(mobile)
+    # 签到
+    t = time.time()
+    time1 = int(round(t * 1000))
+    body_json = {
+        "phone": f"{mobile}",
+        "date": time1,
+        "signSource": "smlprgrm"
+    }
+    body_str = json.dumps(body_json)
+    s = str(encrypt(body_str),'utf-8')
+    sign_body = {
+        "encode": s
+    }
+
+    sign_ret = requests.post(url="https://wapside.189.cn:9001/jt-sign/api/home/sign", json=sign_body,
+                             headers=h5_headers).json()
+    if sign_ret['data']['code'] == 1:
+        msg.append("签到成功, 本次签到获得 " + str(sign_ret['data']['coin']) + " 豆")
+    else:
+        msg.append(sign_ret['data']['msg'])
+
+    msg.append("----------------------------------------------")
+
+def get_h5_headers(mobile):
+    base64_mobile = str(base64.b64encode(mobile[5:11].encode('utf-8')), 'utf-8').strip(r'=+') + "!#!" + str(
+        base64.b64encode(mobile[0:5].encode('utf-8')), 'utf-8').strip(r'=+')
+    return {"User-Agent": "CtClient;9.2.0;Android;10;MI 9;" + base64_mobile}
+
+
+
+def format_msg():
+    str = ''
+    for item in msg:
+        str += item + "\r\n"
+    return str
+
+def main_handler(event, context):
+    for config in config_list:
+        telecom_task(config)
+    content = format_msg()
+
+    send("电信签到任务", content)
+    # 通知
+    #telegram_bot('电信签到任务', content)
+    return content
+
+main_handler(1, 1)
